55c75f7c1a983df3b633786e6076dcd7ca747da4
dancode <dancode@gmail.com>
2016-11-21 13:30:25 -0800

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,16 +4,65 @@
 
 ==============================================================================================*/
 
-/// #include "../../Lib/Core/Core.h"
+pull    thirdparty_lib : tpl    // thirdpartly lib + using namespace
+    
+pull    std_heap                // include the heap module
+pull    std_window
+pull    std_file
+pull    std_log
 
-#include <stdio.h>
+i32     max_values  4           // a read only value.
+   
+// main function passes a commandline argument array.
+main ( string[] argument_array ) i32 exit_code
+{
+    b32 fullscreen, b32 dedicated
+        
+    for ( argument_array : a )
+        
+        if ( a == "dedicated" )
+            dedicated = true
+        if ( a == "fullscreen" )
+            fullscreen = true   
+    ;
+    
+    setup_window( fullscreen )
+    
+    return 1
+}
+    
+// function_name( input params ) output params
+setup_window ( b32 fullscreen ) : b32 success
+{
+    window window
+    window.create( 800, 600, window:current_mode ) : success
+    if !success : return
+    defer window.destroy()
+    
+    window.show( true )
+    window.set_color( 0, 0, 1 )
+    
+    return success;
+}
+    
 
-/*==============================================================================================
+//==============================================================================================
+// Calling Code
 
-int main(const int argc, const char* argv[])
+client prefix : cl
+server prefix : sv
+
+//==============================================================================================
+
+int main( const int argc, const char** argv )
 {
 
     return 0;
 }
 
-/*==============================================================================================
+//==============================================================================================
+
+
+
+
+//==============================================================================================
