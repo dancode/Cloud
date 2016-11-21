78c4346f72ba61b3451bc5db54577f8ea7b0af34
dancode <dancode@gmail.com>
2016-11-21 14:56:33 -0800

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -7,7 +7,7 @@
 pull    thirdparty_lib : tpl    // thirdpartly lib + using namespace
     
 pull    std_heap                // include the heap module
-pull    std_window
+pull    std_window              // 
 pull    std_file
 pull    std_log
 
@@ -17,13 +17,12 @@ i32     max_values  4           // a read only value.
 main ( string[] argument_array ) i32 exit_code
 {
     b32 fullscreen, b32 dedicated
-        
-    for ( argument_array : a )
-        
+    
+    for ( argument_array : a )        
         if ( a == "dedicated" )
             dedicated = true
         if ( a == "fullscreen" )
-            fullscreen = true   
+            fullscreen = true
     ;
     
     setup_window( fullscreen )
@@ -44,7 +43,37 @@ setup_window ( b32 fullscreen ) : b32 success
     
     return success;
 }
-    
+
+struct display_mode
+{
+    u16 width
+    u16 height
+    u16 refresh
+}
+display_mode:default() { return display_mode{ 1280, 758, 0 }}
+display_mode:width() { clamp( width, 640, 1920 ) }
+display_mode:height() { clamp( height, 480, 1080 ) }
+
+DisplayModeSetup()
+{
+    display_mode dm = dm:default()
+}
+
+// Note: use without initialation = auto zero inialization by compiler.
+
+Reverse( array[] array )
+{
+    for ( i < const( array.len / 2 )) i++
+        swap( array[ i ], array[ array.len - i ])
+}
+
+Reverse( char[] array )
+{
+    i = 0, j = array.len - 1;
+    for ( i < j )
+        swap( array[ i ], array[ j ] )
+        i++ j--
+}
 
 //==============================================================================================
 // Calling Code
@@ -56,7 +85,8 @@ server prefix : sv
 
 int main( const int argc, const char** argv )
 {
-
+    
+    
     return 0;
 }
 
